cfd35efddf54a6d973ba9d12c4c5e964190add64
KritishShukla <kritish.shukla0007@gmail.com>
2023-05-18 17:15:26 +0530

    userhandler updated

--- config/UserHandler.js
+++ config/UserHandler.js
@@ -54,3 +54,45 @@ async function fetchProfile(data) {
     }
     console.log("Profile Fetched")
 }
+
+async function enterUser(data){
+
+    try{
+        await User.create({
+            name: data.name.trim(),
+            email: data.email.trim(),
+            libid: data.libid.trim(),
+            phone: data.phone.trim(),
+            userid: profile.data.id,
+            displayname: profile.data.name,
+            username: profile.data.login,
+            avatarUrl: profile.data.avatar_url,
+            profileUrl: profile.data.html_url,
+            gitEmail: profile.data.email,
+            bio: profile.data.bio,
+            blog: profile.data.blog,
+            publicRepo: profile.data.public_repos,
+            followers: profile.data.followers,
+            following: profile.data.following
+        });
+
+        console.log("User added");
+        resp = {
+            status: 200,
+            id: 1,
+            title: "✔Registration Successfull!",
+            message: "Let's make this winter hot!🔥"
+        }
+    }catch(e){
+        console.log(e);
+        resp = {
+            status: 409,
+            id: 3,
+            title: "❌Internal Error Occured",
+            message: "Try contacting team."
+        }
+    }
+}
+
+module.exports = { addUser };
+
