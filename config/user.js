6e0fad541a5cf873f8ea4f94600860dfed1dec0c
KritishShukla <kritish.shukla0007@gmail.com>
2023-05-18 17:12:02 +0530

    user config

---
+++ config/user.js
@@ -0,0 +1,66 @@
+// Schema and Model for User
+
+const mongoose = require('mongoose');
+
+// Login Record
+const loginSchema = new mongoose.Schema({
+    sessionid: String,
+    date: String
+});
+
+// Issue Record
+const issueSchema = new mongoose.Schema({
+    Issue_ID:Number,
+    Issue_number:Number,
+    Issue_title:String,
+    Issue_labels:[String],
+    Issue_difficulty:String,
+    Issue_url:String,
+    Issue_createdAt:String,
+    Issue_updatedAt:String,
+    Issue_closedAt:String
+});
+
+// Scoring Record
+const scoreSchema = new mongoose.Schema({
+    Issue_ID:Number,
+    projectId:String,
+    projectName:String,
+    score:Number,
+    Issue:issueSchema,
+    author_association:String,
+    DBdate:String
+}, { timestamps: true });
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    libid: String,
+    phone: String,
+    userid: String,
+    displayname: String,
+    username: String,
+    score: {
+        type: Number,
+        default: 0
+    },
+    scoresRecord: {
+        type: [scoreSchema],
+        default: []
+    },
+    profileUrl: String,
+    avatarUrl: String,
+    gitEmail: String,
+    bio: String,
+    blog: String,
+    publicRepo: Number,
+    followers: Number,
+    following: Number,
+    sessions: [loginSchema],
+}, { timestamps: true });
+
+
+const User = new mongoose.model('User', userSchema);
+
+module.exports = User;
